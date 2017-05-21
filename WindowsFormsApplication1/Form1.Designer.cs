a474179c2db248276fabb6faf016eeb0fe13725a
Sergey Lyshchenko <lyshchenko.sergiy@gmail.com>
2017-05-21 21:04:16 +0300

    fsd

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьКакToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(823, 452);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(847, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.отрытьToolStripMenuItem,
+            this.сохранитьКакToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // отрытьToolStripMenuItem
+            // 
+            this.отрытьToolStripMenuItem.Name = "отрытьToolStripMenuItem";
+            this.отрытьToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.отрытьToolStripMenuItem.Text = "Отрыть";
+            this.отрытьToolStripMenuItem.Click += new System.EventHandler(this.отрытьToolStripMenuItem_Click);
+            // 
+            // сохранитьКакToolStripMenuItem
+            // 
+            this.сохранитьКакToolStripMenuItem.Name = "сохранитьКакToolStripMenuItem";
+            this.сохранитьКакToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.сохранитьКакToolStripMenuItem.Text = "Сохранить как...";
+            this.сохранитьКакToolStripMenuItem.Click += new System.EventHandler(this.сохранитьКакToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 491);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ЛР";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отрытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьКакToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void function(Bitmap bmp)
+        {
+            for (int i = 0; i < bmp.Width / 2; i++)//R
+            {
+                for (int j = 0; j < bmp.Height / 2; j++)
+                {
+                    int R = bmp.GetPixel(i, j).R;
+                    Color newPix = Color.FromArgb(255, R, 0, 0);
+                    bmp.SetPixel(i, j, newPix);
+                }
+            }
+            for (int i = bmp.Width / 2; i < bmp.Width; i++)//G
+            {
+                for (int j = 0; j < bmp.Height / 2; j++)
+                {
+                    int G = bmp.GetPixel(i, j).G;
+                    Color newPix = Color.FromArgb(255, 0, G, 0);
+                    bmp.SetPixel(i, j, newPix);
+                }
+            }
+            for (int i = 0; i < bmp.Width / 2; i++)//B
+            {
+                for (int j = bmp.Height / 2; j < bmp.Height; j++)
+                {
+                    int B = bmp.GetPixel(i, j).B;
+                    Color newPix = Color.FromArgb(255, 0, 0, B);
+                    bmp.SetPixel(i, j, newPix);
+                }
+            }
+            for (int i = bmp.Width / 2; i < bmp.Width; i++)//gray
+            {
+                for (int j = bmp.Height / 2; j < bmp.Height; j++)
+                {
+                    int R = bmp.GetPixel(i, j).R;
+                    int G = bmp.GetPixel(i, j).G;
+                    int B = bmp.GetPixel(i, j).B;
+                    int Gray = (R + G + B) / 3;
+                    Color newPix = Color.FromArgb(255, Gray, Gray, Gray);
+                    bmp.SetPixel(i, j, newPix);
+                }
+            }
+        }
+        private void отрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.TIF, *.PNG, *.ICO, *.EMF, *.WMF)|*.bmp;*.jpg;*.gif; *.tif; *.png; *.ico; *.emf; *.wmf";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Image image = Image.FromFile(dialog.FileName);
+                int width = image.Width;
+                int height = image.Height;
+                bmp = new Bitmap(image, width, height);
+                function(bmp);
+                pictureBox1.Image = bmp;
+            }
+        }
+        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savedialog = new SaveFileDialog();
+
+            savedialog.Title = "Сохранить картинку как ...";
+            savedialog.OverwritePrompt = true;
+            savedialog.CheckPathExists = true;
+            savedialog.Filter = "Bitmap File(*.bmp)|*.bmp|" + "GIF File(*.gif)|*.gif|" + "JPEG File(*.jpg)|*.jpg|" + "TIF File(*.tif)|*.tif|" + "PNG File(*.png)|*.png";
+            savedialog.ShowHelp = true;
+            if (savedialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = savedialog.FileName;
+                string strFilExtn = fileName.Remove(0, fileName.Length - 3);
+                switch (strFilExtn)
+                {
+                    case "bmp": bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp); break;
+                    case "jpg": bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg); break;
+                    case "gif": bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Gif); break;
+                    case "tif": bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Tiff); break;
+                    case "png": bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Png); break;
+                    default: break;
+                }
+
+            }
+        }
+    }
+}
